c01427b47f1584c7ffa5e5ca99451cd36f96b3d4
Varun <varunkumar.pinnam@gmail.com>
2026-03-20 10:49:00 +0530

    infra

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -6,3 +6,11 @@
 growpart /dev/nvme0n1 4
 lvextend -r -L +30G /dev/mapper/RootVG-homeVol
 xfs_growfs /home
+
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
+
+#cd /home/ec2-user
+#git clone https://github.com/Varun-Kumar-Pinnam/roboshop-infra-dev.git
