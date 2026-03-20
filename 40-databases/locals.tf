bbd2cdf4c63cae1a48bab97b6b713133627bd9d3
Varun <varunkumar.pinnam@gmail.com>
2026-03-20 08:27:05 +0530

    infra

---
+++ 40-databases/locals.tf
@@ -0,0 +1,18 @@
+locals {
+    common_tags = {
+    environment = var.environment
+    project = var.project
+    terraform = "true"
+  }
+
+  final_ec2_tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-mongodb"
+    }
+  )
+   # public subnet in 1a AZ
+  database_subnet_id = split(",",data.aws_ssm_parameter.database_subnet_id.value)[0]
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+
+}
---
+++ 40-databases/main.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "mongodb" {
+  ami           = data.aws_ami.main.id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.mongodb_sg_id]
+
+  tags = local.final_ec2_tags
+}
+/* 
+resource "terraform_data" "mongodb" {
+  triggers_replace = [
+    aws_instance.mongodb.id
+  ]
+# Connection block is required for file provisioners
+connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = DevOps321
+    host     = aws_instance.mongodb.private_ip
+  }
+
+ # Copy a bootstarp file
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+      "sudo chmod +x /tmp/bootstrap.sh"
+     "sudo sh /tmp/bootstrap.sh"
+     ]
+    
+  }
+} */
