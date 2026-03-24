4f19080511c4c5b29de1844ab6d06fda37990376
Varun <varunkumar.pinnam@gmail.com>
2026-03-24 16:01:25 +0530

    infra

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -18,5 +18,5 @@ ec2_final_tags = merge(
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_id.value)[0]
 
   zone_id = data.aws_route53_zone.selected.zone_id
-  vpc_id = data.aws_ssm_parameter.vpc_id
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
 }
