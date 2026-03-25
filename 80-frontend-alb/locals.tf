2af15483d086794e87c1313168aba61d36d70234
Varun <varunkumar.pinnam@gmail.com>
2026-03-25 16:51:30 +0530

    infra

--- 80-frontend-alb/locals.tf
+++ 80-frontend-alb/locals.tf
@@ -8,7 +8,7 @@ locals {
   }
 
   frontend_alb_sg_id  = data.aws_ssm_parameter.frontend_alb_sg_id.value
-  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_id.value)
   frontend_alb_certificate_arn=data.aws_ssm_parameter.frontend_alb_certificate_arn.value
   zone_id = data.aws_route53_zone.selected.zone_id
 }
