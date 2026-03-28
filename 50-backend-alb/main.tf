ff229d9d1d09908095fa1681dfd6f91233ef84b1
Varun <varunkumar.pinnam@gmail.com>
2026-03-28 07:14:26 +0530

    infra

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -38,7 +38,7 @@ resource "aws_route53_record" "www" {
   name    = "*.backend-alb-${var.environment}.${var.domain_name}"
   type    = "A"
 
-  # releated to load balancer
+  # releated to load balancer   
   alias {
     name                   = aws_lb.backend_alb.dns_name
     zone_id                = aws_lb.backend_alb.zone_id
